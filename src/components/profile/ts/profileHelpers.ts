cce5c5654c3ba84b5c2109ecfa656fc352b63307
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-07-07 12:08:52 +0200

    refactor: 💡 renaming of profile views and functions

--- src/components/profile/ts/profileHelpers.ts
+++ src/components/profile/ts/profileHelpers.ts
@@ -1,29 +1,5 @@
-import { SessionUserDataInterface } from '../../../globalState';
-import { array } from 'prop-types';
-
-//TODO: rm on profile refactor
-export interface ProfileData {
-	absenceMessage: string;
-	absent: boolean;
-	agencies: [
-		{
-			id: number;
-			name: string;
-			postcode: number;
-			description: string;
-			teamAgency: false;
-		}
-	];
-	email: string;
-	firstName: string;
-	lastName: string;
-	inTeamAgency: boolean;
-	userName: string;
-	userRoles: Array<string>;
-	userId: string;
-}
-
 export const convertUserDataObjectToArray = (object) => {
+	console.log('convert', object);
 	const array = [];
 	Object.keys(object).map(function (key) {
 		let test = {
