07f3a3065575cee0f904ea39dc16387ac4a48043
Robin Spielmann <robin.spielmann@virtual-identity.com>
2020-07-07 10:15:12 +0200

    refactor: 💡 add helper functions and small adjustments

--- src/components/messageSubmitInterface/ts/messageSubmitInterfaceComponent.tsx
+++ src/components/messageSubmitInterface/ts/messageSubmitInterfaceComponent.tsx
@@ -52,6 +52,10 @@ import {
 	UnorderedListButton
 } from 'draft-js-buttons';
 import createEmojiPlugin from 'draft-js-emoji-plugin';
+import {
+	emojiPickerCustomClasses,
+	toolbarCustomClasses
+} from './richtextHelpers';
 
 const INPUT_MAX_LENGTH = 7500;
 
@@ -60,15 +64,7 @@ const linkifyPlugin = createLinkifyPlugin();
 
 //Static Toolbar Plugin
 const staticToolbarPlugin = createToolbarPlugin({
-	theme: {
-		toolbarStyles: {
-			toolbar: 'textarea__toolbar'
-		},
-		buttonStyles: {
-			button: 'textarea__toolbar__button',
-			active: 'textarea__toolbar__button--active'
-		}
-	}
+	theme: toolbarCustomClasses
 });
 const { Toolbar } = staticToolbarPlugin;
 
@@ -92,41 +88,7 @@ const emojiSelectButtonIcon = (
 );
 
 const emojiPlugin = createEmojiPlugin({
-	theme: {
-		emojiSelect: 'emoji__select',
-		emojiSelectButton: 'emoji__selectButton',
-		emojiSelectButtonPressed: 'emoji__selectButton--pressed',
-		emojiSelectPopover: 'emoji__selectPopover',
-		emojiSelectPopoverClosed: 'emoji__selectPopover--closed',
-		emojiSelectPopoverTitle: 'emoji__selectPopover__title',
-		emojiSelectPopoverGroups: 'emoji__selectPopover__groups',
-		emojiSelectPopoverGroup: 'emoji__selectPopover__group',
-		emojiSelectPopoverGroupTitle: 'emoji__selectPopover__groupTitle',
-		emojiSelectPopoverGroupList: 'emoji__selectPopover__groupList',
-		emojiSelectPopoverGroupItem: 'emoji__selectPopover__groupItem',
-		emojiSelectPopoverToneSelect: 'emoji__selectPopover__toneSelect',
-		emojiSelectPopoverToneSelectList:
-			'emoji__selectPopover__toneSelectList',
-		emojiSelectPopoverToneSelectItem:
-			'emoji__selectPopover__toneSelectItem',
-		emojiSelectPopoverEntry: 'emoji__selectPopover__entry',
-		emojiSelectPopoverEntryFocused: 'emoji__selectPopover__entry--focused',
-		emojiSelectPopoverEntryIcon: 'emoji__selectPopover__entryIcon',
-		emojiSelectPopoverNav: 'emoji__selectPopover__nav',
-		emojiSelectPopoverNavItem: 'emoji__selectPopover__navItem',
-		emojiSelectPopoverNavEntry: 'emoji__selectPopover__navEntry',
-		emojiSelectPopoverNavEntryActive:
-			'emoji__selectPopover__navEntry--active',
-		emojiSelectPopoverScrollbar: 'emoji__selectPopover__scrollbar',
-		emojiSelectPopoverScrollbarThumb:
-			'emoji__selectPopover__scrollbarThumb',
-		emoji: 'emoji',
-		emojiSuggestionsEntry: 'emoji__suggestionsEntry',
-		emojiSuggestionsEntryFocused: 'emoji__suggestionsEntry--focused',
-		emojiSuggestionsEntryText: 'emoji__suggestionsEntry__text',
-		emojiSuggestionsEntryIcon: 'emoji__suggestionsEntry__icon',
-		emojiSuggestions: 'emoji__suggestions'
-	},
+	theme: emojiPickerCustomClasses,
 	useNativeArt: true,
 	selectButtonContent: emojiSelectButtonIcon
 });
@@ -358,7 +320,6 @@ export const MessageSubmitInterfaceComponent = (
 		const currentContentLength = currentContent.getPlainText('').length;
 
 		if (currentContentLength > INPUT_MAX_LENGTH - 1) {
-			console.log('you can type max ten characters');
 			return 'handled';
 		}
 	};
@@ -368,7 +329,6 @@ export const MessageSubmitInterfaceComponent = (
 		const currentContentLength = currentContent.getPlainText('').length;
 
 		if (currentContentLength + pastedText.length > INPUT_MAX_LENGTH) {
-			console.log('you can paste max ten characters');
 			return 'handled';
 		}
 	};
---
+++ src/components/messageSubmitInterface/ts/richtextHelpers.ts
@@ -0,0 +1,41 @@
+export const emojiPickerCustomClasses = {
+	emojiSelect: 'emoji__select',
+	emojiSelectButton: 'emoji__selectButton',
+	emojiSelectButtonPressed: 'emoji__selectButton--pressed',
+	emojiSelectPopover: 'emoji__selectPopover',
+	emojiSelectPopoverClosed: 'emoji__selectPopover--closed',
+	emojiSelectPopoverTitle: 'emoji__selectPopover__title',
+	emojiSelectPopoverGroups: 'emoji__selectPopover__groups',
+	emojiSelectPopoverGroup: 'emoji__selectPopover__group',
+	emojiSelectPopoverGroupTitle: 'emoji__selectPopover__groupTitle',
+	emojiSelectPopoverGroupList: 'emoji__selectPopover__groupList',
+	emojiSelectPopoverGroupItem: 'emoji__selectPopover__groupItem',
+	emojiSelectPopoverToneSelect: 'emoji__selectPopover__toneSelect',
+	emojiSelectPopoverToneSelectList: 'emoji__selectPopover__toneSelectList',
+	emojiSelectPopoverToneSelectItem: 'emoji__selectPopover__toneSelectItem',
+	emojiSelectPopoverEntry: 'emoji__selectPopover__entry',
+	emojiSelectPopoverEntryFocused: 'emoji__selectPopover__entry--focused',
+	emojiSelectPopoverEntryIcon: 'emoji__selectPopover__entryIcon',
+	emojiSelectPopoverNav: 'emoji__selectPopover__nav',
+	emojiSelectPopoverNavItem: 'emoji__selectPopover__navItem',
+	emojiSelectPopoverNavEntry: 'emoji__selectPopover__navEntry',
+	emojiSelectPopoverNavEntryActive: 'emoji__selectPopover__navEntry--active',
+	emojiSelectPopoverScrollbar: 'emoji__selectPopover__scrollbar',
+	emojiSelectPopoverScrollbarThumb: 'emoji__selectPopover__scrollbarThumb',
+	emoji: 'emoji',
+	emojiSuggestionsEntry: 'emoji__suggestionsEntry',
+	emojiSuggestionsEntryFocused: 'emoji__suggestionsEntry--focused',
+	emojiSuggestionsEntryText: 'emoji__suggestionsEntry__text',
+	emojiSuggestionsEntryIcon: 'emoji__suggestionsEntry__icon',
+	emojiSuggestions: 'emoji__suggestions'
+};
+
+export const toolbarCustomClasses = {
+	toolbarStyles: {
+		toolbar: 'textarea__toolbar'
+	},
+	buttonStyles: {
+		button: 'textarea__toolbar__button',
+		active: 'textarea__toolbar__button--active'
+	}
+};
