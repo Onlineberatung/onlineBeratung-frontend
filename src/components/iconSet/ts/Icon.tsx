b88c15cac2f8b7af56fcab8231839da0359b7237
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-07-31 14:57:41 +0200

    feat: 🎸 implemented icon component with variants

---
+++ src/components/iconSet/ts/Icon.tsx
@@ -0,0 +1,61 @@
+import {
+	IconEnvelope,
+	IconLock,
+	IconPerson,
+	IconPin,
+	IconCaritasWhite,
+	IconInVia,
+	IconKreuzbund,
+	IconRaphaelswerk,
+	IconSKF,
+	IconSKM
+} from './index';
+import React from 'react';
+
+export const ICON_KEYS = {
+	ENVELOPE: 'ENVELOPE',
+	LOCK: 'LOCK',
+	PERSON: 'PERSON',
+	PIN: 'PIN'
+};
+
+export const LOGO_KEYS = {
+	CARITAS_WHITE: 'CARITAS_WHITE',
+	IN_VIA: 'IN_VIA',
+	KREUZBUND: 'KREUZBUND',
+	RAPHAELSWERK: 'RAPHAELSWERK',
+	SKF: 'SKF',
+	SKM: 'SKM'
+};
+
+export interface IconProps {
+	name: string;
+	className?: string;
+}
+
+export const Icon = (props: IconProps) => {
+	switch (props.name) {
+		case ICON_KEYS.ENVELOPE:
+			return <IconEnvelope {...props} />;
+		case ICON_KEYS.LOCK:
+			return <IconLock {...props} />;
+		case ICON_KEYS.PERSON:
+			return <IconPerson {...props} />;
+		case ICON_KEYS.LOCK:
+			return <IconPin {...props} />;
+		case LOGO_KEYS.CARITAS_WHITE:
+			return <IconCaritasWhite {...props} />;
+		case LOGO_KEYS.IN_VIA:
+			return <IconInVia {...props} />;
+		case LOGO_KEYS.KREUZBUND:
+			return <IconKreuzbund {...props} />;
+		case LOGO_KEYS.RAPHAELSWERK:
+			return <IconRaphaelswerk {...props} />;
+		case LOGO_KEYS.SKF:
+			return <IconSKF {...props} />;
+		case LOGO_KEYS.SKM:
+			return <IconSKM {...props} />;
+		default:
+			return;
+	}
+};
---
+++ src/components/iconSet/ts/index.ts
@@ -0,0 +1,11 @@
+export * from './icons/IconEnvelope';
+export * from './icons/IconLock';
+export * from './icons/IconPerson';
+export * from './icons/IconPin';
+
+export * from './logos/IconCaritasWhite';
+export * from './logos/IconInVia';
+export * from './logos/IconKreuzbund';
+export * from './logos/IconRaphaelswerk';
+export * from './logos/IconSKF';
+export * from './logos/IconSKM';
