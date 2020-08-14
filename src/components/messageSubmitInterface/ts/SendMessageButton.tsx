f646f8d6c4797c17f4bef1cd1cf22672fec19b07
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-08-14 08:58:01 +0200

    refactor: 💡 refactor styles, remove deprecated

--- src/components/messageSubmitInterface/ts/SendMessageButton.tsx
+++ src/components/messageSubmitInterface/ts/SendMessageButton.tsx
@@ -11,14 +11,13 @@ export const SendMessageButton = (props: SendMessageButtonProps) => {
 	return (
 		<span
 			onClick={() => props.handleSendButton()}
-			className={`inputIcon__wrapper ${
-				props.clicked ? 'inputIcon__wrapper--clicked' : ''
-			} ${props.deactivated ? 'inputIcon__wrapper--deactivated' : ''}`}
+			className={`textarea__iconWrapper ${
+				props.clicked ? 'textarea__iconWrapper--clicked' : ''
+			} ${props.deactivated ? 'textarea__iconWrapper--deactivated' : ''}`}
 			title={translate('enquiry.write.input.button.title')}
 		>
 			<svg
-				id="sendMessage"
-				className="textarea__icon inputField__icon"
+				className="textarea__icon"
 				xmlns="http://www.w3.org/2000/svg"
 				xmlnsXlink="http://www.w3.org/1999/xlink"
 				width="72"
