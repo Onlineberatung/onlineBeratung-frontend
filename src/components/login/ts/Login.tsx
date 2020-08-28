ea4e3400af6e9af204e8a4649dd1e4a57c9dfa47
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-08-28 14:04:59 +0200

    refactor: 💡 revert file name change

--- src/components/login/ts/Login.tsx
+++ src/components/login/ts/Login.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Stage } from '../../stage/ts/Stage';
+import { Stage } from '../../stage/ts/stage';
 import { translate } from '../../../resources/ts/i18n/translate';
 import { InputField, InputFieldItemTSX } from '../../inputField/ts/InputField';
 import { useState, useEffect } from 'react';
