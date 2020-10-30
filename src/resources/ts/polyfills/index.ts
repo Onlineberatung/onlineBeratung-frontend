a1a7bc3d2ac3e31be3a3c85799b90616a701158e
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-10-30 14:38:37 +0100

    fix: 🐛 ie 11 polyfill

--- src/resources/ts/polyfills/index.ts
+++ src/resources/ts/polyfills/index.ts
@@ -2,7 +2,7 @@ import './ChildNode.remove';
 import './Element.classList';
 import './Element.closest';
 import './NodeList.forEach';
-import './TextEncoder';
 
 import 'element-scroll-polyfill';
 import 'fetch-polyfill/fetch';
+import 'text-encoding';
