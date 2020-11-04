d3c60b2e70a603f0137c07cf794aca1b4d721d65
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2020-11-04 18:17:54 +0100

    refactor: 💡 added polyfills ie

--- src/resources/ts/polyfills/index.ts
+++ src/resources/ts/polyfills/index.ts
@@ -2,7 +2,8 @@ import './ChildNode.remove';
 import './Element.classList';
 import './Element.closest';
 import './NodeList.forEach';
+import './TextEncoder';
 
 import 'element-scroll-polyfill';
+import 'fastestsmallesttextencoderdecoder';
 import 'fetch-polyfill/fetch';
-import 'text-encoding';
