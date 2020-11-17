cac00ed0096b515ddc21c75dc3baf3355ec2bf83
Johannes Becker <johannes.becker+cpk@gmail.com>
2020-11-17 10:02:50 +0100

    feat(polyfill): ie 11 support

---
+++ src/polyfill.ts
@@ -0,0 +1,10 @@
+import 'react-app-polyfill/stable';
+import 'fastestsmallesttextencoderdecoder';
+import 'whatwg-fetch';
+import 'core-js/features/promise';
+import 'core-js/web/dom-collections';
+import 'element-remove';
+import 'element-scroll-polyfill';
+import elementClosest from 'element-closest';
+
+elementClosest(window);
