91e2b1d142b30fbadfc069bee690d4666bd8106b
Robin Spielmann <robin.spielmann@virtual-identity.com>
2020-12-21 16:15:55 +0100

    test: 💍 restructure session list tests

--- cypress/integration/session-list.spec.ts
+++ cypress/integration/session-list.spec.ts
@@ -19,71 +19,41 @@ describe('session list', () => {
 			);
 		});
 
-		// describe(-> feature wie illustration oder nachladen von sessions) anstelle von Anzahl der Sessions
-		// it -> should show with given limit of sessions -> im Test eine config Variable ziehen, die auch die Anzahl der Sessions im production code bestimmt (ähnlich wie bei den Translations)
-		// it -> should not show with given constraints (siehe oben)
-
-		describe('no sessions', () => {
-			describe('the user is an consultant', () => {
-				// info text shown
-			});
+		it('should display all existing sessions (3 sessions)', () => {
+			const sessionCount = 3;
+			const sessions = generateMultipleSessions(sessionCount);
+			cy.caritasMockedLogin({ sessions });
+			cy.get('[data-cy=session-list-item]').should(
+				'have.length',
+				sessionCount
+			);
 		});
 
-		describe('3 sessions', () => {
-			it('should display all existing sessions', () => {
-				const sessionCount = 3;
-				const sessions = generateMultipleSessions(sessionCount);
-				cy.caritasMockedLogin({ sessions });
-				cy.get('[data-cy=session-list-item]').should(
-					'have.length',
-					sessionCount
-				);
-			});
+		it('should display all existing sessions (100 sessions)', () => {
+			const sessionCount = 100;
+			const sessions = generateMultipleSessions(sessionCount);
+			cy.caritasMockedLogin({ sessions });
+			cy.get('[data-cy=session-list-item]').should(
+				'have.length',
+				sessionCount
+			);
+		});
 
-			it('should show a welcome illustration', () => {
+		describe('welcome illustration', () => {
+			it('should show with 3 or less sessions', () => {
 				const sessions = generateMultipleSessions(3);
 				cy.caritasMockedLogin({ sessions });
 				cy.get('[data-cy=session-list-welcome-illustration]').should(
 					'exist'
 				);
 			});
-		});
 
-		describe('4 sessions', () => {
-			it('should display all existing sessions', () => {
-				const sessionCount = 4;
-				const sessions = generateMultipleSessions(sessionCount);
-				cy.caritasMockedLogin({ sessions });
-				cy.get('[data-cy=session-list-item]').should(
-					'have.length',
-					sessionCount
-				);
-			});
-
-			it('should not show a welcome illustration', () => {
+			it('should not show with 4 or more sessions', () => {
 				const sessions = generateMultipleSessions(4);
 				cy.caritasMockedLogin({ sessions });
 				cy.get('[data-cy=session-list-welcome-illustration]').should(
 					'not.exist'
 				);
-			});
-		});
-
-		describe('20 sessions', () => {
-			describe('the user is an asker', () => {
-				it('should display all existing sessions', () => {
-					const sessionCount = 20;
-					const sessions = generateMultipleSessions(sessionCount);
-					cy.caritasMockedLogin({ sessions });
-					cy.get('[data-cy=session-list-item]').should(
-						'have.length',
-						sessionCount
-					);
-				});
-			});
-
-			describe('the user is an consultant', () => {
-				// only 15 items are loaded
 			});
 		});
 	});
