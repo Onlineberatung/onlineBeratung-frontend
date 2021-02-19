0159a89986005b29cc0b1f427f4321c09ba55dae
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-02-19 14:28:13 +0100

    style: 💄 remove unused style file

--- src/components/registration/RegistrationUsername.tsx
+++ src/components/registration/RegistrationUsername.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import './registrationUsername.styles';
 import { translate } from '../../resources/scripts/i18n/translate';
 import {
 	InputField,
