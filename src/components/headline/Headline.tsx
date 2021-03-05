f2c03568aada547331866df57343bcda4a8f98b1
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2021-03-05 12:03:55 +0100

    feat: 🎸 added <Headline />

---
+++ src/components/headline/Headline.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './headline.styles';
+
+interface HeadlineProps {
+	semanticLevel: '1' | '2' | '3' | '4' | '5';
+	styleLevel?: 'super' | '1' | '2' | '3' | '4' | '5';
+	text: string;
+}
+
+export const Headline = (props: HeadlineProps) => {
+	const Tag = ('h' + props.semanticLevel) as keyof JSX.IntrinsicElements;
+	const levelBasedClass = props.styleLevel
+		? props.styleLevel
+		: props.semanticLevel;
+
+	return (
+		<Tag className={`headline headline--${levelBasedClass}`}>
+			{props.text}
+		</Tag>
+	);
+};
