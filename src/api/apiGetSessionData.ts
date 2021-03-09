51593ac2d1c02661840de64028adb3fbc734263f
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-03-09 16:19:34 +0100

    style: 💄 remove unused url params in getSessionData call

--- src/api/apiGetSessionData.ts
+++ src/api/apiGetSessionData.ts
@@ -2,14 +2,8 @@ import { config } from '../resources/scripts/config';
 import { FETCH_METHODS, fetchData, FETCH_ERRORS } from './fetchData';
 
 // TODO: remove offset and count out of URL on api change
-export const apiGetSessionData = async (
-	rcGroupId: string,
-	offset: number = 0,
-	count: number = 0
-): Promise<any> => {
-	const url =
-		config.endpoints.messages +
-		`?offset=${offset}&count=${count}&rcGroupId=${rcGroupId}`;
+export const apiGetSessionData = async (rcGroupId: string): Promise<any> => {
+	const url = config.endpoints.messages + `?rcGroupId=${rcGroupId}`;
 
 	return fetchData({
 		url: url,
