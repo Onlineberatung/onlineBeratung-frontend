0bec0a69b6581238eb9ae5ea63d25a8aa84918c7
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-03-11 08:18:54 +0100

    style: 💄 remove old comment

--- src/api/apiGetSessionData.ts
+++ src/api/apiGetSessionData.ts
@@ -1,7 +1,6 @@
 import { config } from '../resources/scripts/config';
 import { FETCH_METHODS, fetchData, FETCH_ERRORS } from './fetchData';
 
-// TODO: remove offset and count out of URL on api change
 export const apiGetSessionData = async (rcGroupId: string): Promise<any> => {
 	const url = config.endpoints.messages + `?rcGroupId=${rcGroupId}`;
 
