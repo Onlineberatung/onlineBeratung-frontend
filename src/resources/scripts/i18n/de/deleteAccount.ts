60cfff3e63d7fb2bae270d94ffef91687e23872a
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-03-16 11:47:52 +0100

    style: 💄 change wording on delete account overlay

--- src/resources/scripts/i18n/de/deleteAccount.ts
+++ src/resources/scripts/i18n/de/deleteAccount.ts
@@ -2,7 +2,7 @@ const deleteAccount = {
 	'button.label': 'Account löschen',
 	'confirmOverlay.headline': 'Möchten Sie Ihren Account wirklich löschen?',
 	'confirmOverlay.copy':
-		'<strong>Dieser Vorgang kann nicht rückgängig gemacht werden.</strong><br><br>Ihr Account wird innerhalb der nächsten 48 Stunden gelöscht. Ihre Daten werden gemäß der geltenden Datenschutzbestimmungen aufbewahrt bzw. gelöscht.<br><br>Bitte geben Sie Ihr Passwort ein, um Ihren Account nun zu löschen.',
+		'<strong>Dieser Vorgang kann nicht rückgängig gemacht werden.</strong><br><br>Ihr Account wird innerhalb der nächsten 48 Stunden gelöscht. Ihre Daten werden gemäß der geltenden Datenschutzbestimmungen gelöscht.<br><br>Bitte geben Sie Ihr Passwort ein, um Ihren Account nun zu löschen.',
 	'confirmOverlay.button.confirm': 'ja',
 	'confirmOverlay.button.deny': 'nein',
 	'confirmOverlay.input.label': 'Passwort',
