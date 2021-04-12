ed5f3fddbce077f878ecfc2e9de109171928b4bd
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-04-12 17:25:19 +0200

    fix: 🐛 adjust startvideocall call based on new response

--- src/components/sessionMenu/SessionMenu.tsx
+++ src/components/sessionMenu/SessionMenu.tsx
@@ -193,7 +193,7 @@ export const SessionMenu = () => {
 		label: translate('chatFlyout.feedback')
 	};
 
-	const hasVideoCallFeatures = () => false;
+	const hasVideoCallFeatures = () => true;
 	// TODO: reimplement on videocall release
 	// !isGroupChat &&
 	// !typeIsEnquiry(getTypeOfLocation()) &&
@@ -204,9 +204,8 @@ export const SessionMenu = () => {
 		apiStartVideoCall(chatItem.id)
 			.then((response) => {
 				videoCallWindow.location.href = getVideoCallUrl(
-					response.videoCallUrl,
-					isVideoActivated,
-					true
+					response.moderatorVideoCallUrl,
+					isVideoActivated
 				);
 				videoCallWindow.focus();
 			})
