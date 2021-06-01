ed97e49431fd1ac95e47b9ed3d52482fa2592799
Jan Amann <jan@amann.me>
2021-06-01 16:07:58 +0200

    feat: improve error page styling and use translations

---
+++ src/resources/scripts/i18n/de/error.ts
@@ -0,0 +1,16 @@
+const error = {
+	'title': 'Beratung & Hilfe',
+	'claim': 'Online. Anonym. Sicher.',
+	'statusCodes.401.headline': 'Schade!',
+	'statusCodes.401.description':
+		'Leider sind Sie nicht berechtigt diese Seite  einzusehen.<br>Bitte melden Sie sich an.',
+	'statusCodes.404.headline': 'Ohh!',
+	'statusCodes.404.description':
+		'Es tut uns leid da ist wohl etwas schief gelaufen.<br>Wir konnten die gewünschte Seite nicht finden.',
+	'statusCodes.500.headline': 'Ups!',
+	'statusCodes.500.description':
+		'Wie es aussieht haben wir momentan ein Serverproblem.<br>Versuchen Sie es später noch einmal.',
+	'login': 'Zum Login'
+};
+
+export default error;
