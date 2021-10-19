3a76ba41994e7c074c9366d310c2ab39cdcf30b8
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-10-19 10:58:51 +0200

    feat: 🎸 add info message for archived sessions

--- src/resources/scripts/i18n/de/archiveInformal.ts
+++ src/resources/scripts/i18n/de/archiveInformal.ts
@@ -2,7 +2,9 @@ const archiveInformal = {
 	'overlay.session.success':
 		'Du hast den Ratsuchenden erfolgreich archiviert.',
 	'overlay.teamsession.success':
-		'Du hast den Ratsuchenden für dich und dein Team erfolgreich archiviert.'
+		'Du hast den Ratsuchenden für dich und dein Team erfolgreich archiviert.',
+	'submitInfo.message':
+		'Sobald Du oder der Ratsuchende eine Nachricht schreibt, wird der Nachrichtenverlauf automatisch wieder in die Liste der Ratsuchenden eingeordnet.'
 };
 
 export default archiveInformal;
