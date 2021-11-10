657b783803a56a6ef3d32fc283d711b5885e2b94
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-10 16:16:40 +0100

    feat: capacitor setup

---
+++ android/app/src/main/java/com/CaritasOnlineBeratung/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.CaritasOnlineBeratung.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Caritas-OnlineBeratung.app',
+  appName: 'Caritas Online-Beratung',
+  webDir: './src/pages',
+  bundledWebRuntime: false
+};
+
+export default config;
