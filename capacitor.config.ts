7e30b0a6d76d36a2be935a788d63fce81e0bdd92
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-11 11:05:30 +0100

    feature: add ios platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Caritas-OnlineBeratung.app',
+  appId: 'com.CaritasOnlineBeratung.app',
   appName: 'Caritas Online-Beratung',
   webDir: './src/pages',
   bundledWebRuntime: false
