044e1e4694025c28f6149dede875a86f8a152d63
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-12 12:20:59 +0100

    fix: change capacitor.config datatype

---
+++ ios/App/App/capacitor.config.ts
@@ -0,0 +1,8 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: "com.CaritasOnlineBeratung.app",
+	appName: "Caritas Online-Beratung",
+	webDir: "./src/pages",
+}
+export default config;
