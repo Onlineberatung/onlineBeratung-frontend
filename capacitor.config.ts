a95db840f1a843fc0b6f40071c773be852aa9a34
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-18 15:06:14 +0100

    fix: apply prettier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.caritas.onlineberatung.app',
-  appName: 'Caritas Online-Beratung',
-  bundledWebRuntime: false,
-  webDir: './build',
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,
-      androidScaleType: "CENTER_CROP"
-    }
-  },
-  server: {
-    url: "https://caritas-dev.virtual-identity.com/",
-    cleartext: true
-},
-cordova: {}
+	appId: 'de.caritas.onlineberatung.app',
+	appName: 'Caritas Online-Beratung',
+	bundledWebRuntime: false,
+	webDir: './build',
+	plugins: {
+		SplashScreen: {
+			launchShowDuration: 0,
+			androidScaleType: 'CENTER_CROP'
+		}
+	},
+	server: {
+		url: 'https://caritas-dev.virtual-identity.com/',
+		cleartext: true
+	},
+	cordova: {}
 };
 
 export default config;
