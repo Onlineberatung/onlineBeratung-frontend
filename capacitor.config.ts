f37c90b63e92b2c6d99e533a9a37cf20e287763e
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-26 14:15:13 +0100

    feat: capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.caritas.onlineberatung.app',
+  appName: 'Caritas Online-Beratung',
+  webDir: './build',
+  bundledWebRuntime: false
+};
+
+export default config;
