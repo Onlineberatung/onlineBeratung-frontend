8bff0178e4f7c2a3ce636cbda282f7b210388a03
niklasb99 <niklas.burger@virtual-identity.com>
2021-11-26 14:41:23 +0100

    feat: access to web url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,20 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.caritas.onlineberatung.app',
-  appName: 'Caritas Online-Beratung',
-  webDir: './build',
-  bundledWebRuntime: false
+	appId: 'de.caritas.onlineberatung.app',
+	appName: 'Caritas Online-Beratung',
+	bundledWebRuntime: false,
+	webDir: './build',
+	// plugins: {
+	// 	SplashScreen: {
+	// 		launchShowDuration: 0,
+	// 		androidScaleType: 'CENTER_CROP'
+	// 	}
+	// },
+	server: {
+		url: 'https://caritas-dev.virtual-identity.com/',
+		cleartext: true
+	},
 };
 
 export default config;
