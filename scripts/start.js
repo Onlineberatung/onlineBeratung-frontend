97e4f6767c17a67d2cf4fd0aaa691783a39e08ad
Jan Amann <jan@amann.me>
2022-01-17 10:37:29 +0100

    chore: upgrade remaining build dependencies

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@
 
 // Set REACT_APP_API_URL and REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY for local development against a server
 const dotenv = require('dotenv');
-const dotenvExpand = require('dotenv-expand');
+const dotenvExpand = require('dotenv-expand').expand;
 dotenvExpand(dotenv.config());
 process.env.REACT_APP_API_URL = process.env.API_URL_FOR_LOCAL_DEVELOPMENT;
 process.env.REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY =
