3e5686f5916ea491ebd2ee58a72e87a9f7f2b30c
niklasb99 <niklas.burger@virtual-identity.com>
2022-01-26 15:32:52 +0100

    fix: optimize error handling

--- src/utils/biometricAuthenticationHelpers.ts
+++ src/utils/biometricAuthenticationHelpers.ts
@@ -8,20 +8,24 @@ import { apiUrl } from '../resources/scripts/config';
 //checks if the currently used device provides the biometric authentication function
 export const checkForBiometricAvailability = (
 	handleAvailableBiometrics: Function,
-	handleUnavailableBiometrics?: Function
+	handleUnavailableBiometrics?: Function,
+	handleBiometricsError?: Function
 ) => {
-	NativeBiometric.isAvailable().then(
-		(result: AvailableResult) => {
-			if (result.isAvailable) {
+	NativeBiometric.isAvailable()
+		.then((result: AvailableResult) => {
+			if (result.isAvailable || result.biometryType !== 0) {
 				handleAvailableBiometrics();
+			} else {
+				if (handleUnavailableBiometrics) {
+					handleUnavailableBiometrics();
+				}
 			}
-		},
-		(error) => {
-			if (handleUnavailableBiometrics) {
-				handleUnavailableBiometrics();
+		})
+		.catch((err) => {
+			if (handleBiometricsError) {
+				handleBiometricsError();
 			}
-		}
-	);
+		});
 };
 
 //checks whether the user has already activated the biometric authentication function in his profile on the currently used device.
@@ -50,37 +54,32 @@ export const checkForExistingCredentials = (
 //checks if the user can authenticate with a fingerprint or FaceID stored on the currently used device
 export const checkIdentity = (
 	handleCheckIdentityCancel: Function,
-	handleCheckIdentityError: Function,
 	handleCheckIdentitySuccess?: Function,
 	addBlurEffect?: Function
 ) => {
 	NativeBiometric.getCredentials({
 		server: apiUrl
-	})
-		.then(
-			(credentials: Credentials) =>
-				new Promise((reject) => {
-					if (addBlurEffect) {
-						addBlurEffect();
+	}).then(
+		(credentials: Credentials) =>
+			new Promise((reject) => {
+				if (addBlurEffect) {
+					addBlurEffect();
+				}
+				NativeBiometric.verifyIdentity({
+					reason: 'For easy log in',
+					title: 'Log in',
+					subtitle: 'Maybe add subtitle here?',
+					description: 'Maybe a description too?'
+				}).then(
+					() => {
+						if (handleCheckIdentitySuccess) {
+							handleCheckIdentitySuccess(credentials);
+						}
+					},
+					(error) => {
+						handleCheckIdentityCancel(error);
 					}
-					NativeBiometric.verifyIdentity({
-						reason: 'For easy log in',
-						title: 'Log in',
-						subtitle: 'Maybe add subtitle here?',
-						description: 'Maybe a description too?'
-					})
-						.then(() => {
-							if (handleCheckIdentitySuccess) {
-								handleCheckIdentitySuccess(credentials);
-							}
-						})
-
-						.catch((reject) => {
-							handleCheckIdentityCancel();
-						});
-				})
-		)
-		.catch((err) => {
-			handleCheckIdentityError();
-		});
+				);
+			})
+	);
 };
