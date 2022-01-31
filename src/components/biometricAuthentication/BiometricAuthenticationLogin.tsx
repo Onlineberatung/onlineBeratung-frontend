5babc4b8ce7b10b4d542610228a85f22b8f0c647
niklasb99 <niklas.burger@virtual-identity.com>
2022-01-31 09:46:28 +0100

    fix: optimize BioAuth behavior in login area when app closed and opened again

--- src/components/biometricAuthentication/BiometricAuthenticationLogin.tsx
+++ src/components/biometricAuthentication/BiometricAuthenticationLogin.tsx
@@ -11,24 +11,13 @@ import { App } from '@capacitor/app';
 export const BiometricAuthenticationLogin = () => {
 	const [isAppActive, setIsAppActive] = useState<boolean>(true);
 	const isBiometricDialogOpen = useRef<boolean>(false);
-
-	const stageAnimation = () => {
-		let stage = document.getElementById('loginLogoWrapper');
-		stage?.classList.add('stage--ready');
-	};
+	const [isAnimated, setIsAnimated] = useState<boolean>(false);
 
 	useEffect(() => {
 		if (isAppActive && !isBiometricDialogOpen.current) {
 			isBiometricDialogOpen.current = true;
-			checkForBiometricAvailability(
-				handleAvailableBiometrics,
-				() => {
-					stageAnimation();
-				},
-				() => {
-					stageAnimation();
-				}
-			);
+			console.log('Login: checkForBiometricAvailability');
+			checkForBiometricAvailability(handleAvailableBiometrics);
 		} else if (isAppActive && isBiometricDialogOpen.current) {
 			isBiometricDialogOpen.current = false;
 		}
@@ -44,23 +33,33 @@ export const BiometricAuthenticationLogin = () => {
 	}, []);
 
 	const handleAvailableBiometrics = () => {
-		checkForExistingCredentials(handleCredentials, () => {
-			stageAnimation();
-		});
+		console.log('Login: checkForExistingCredentials');
+		checkForExistingCredentials(handleCredentials);
 	};
 
 	const handleCredentials = (hasCredentialsSet: Boolean) => {
 		if (hasCredentialsSet) {
-			checkIdentity((error) => {
-				if (error.code !== '15') {
-					stageAnimation();
-				} else {
-					isBiometricDialogOpen.current = false;
-				}
-			}, handleCheckIdentitySuccess);
-		} else {
-			stageAnimation();
+			//if the stage hasn't already been animated, the authenticationProcess Function will be executed after 3.5 sec - wait until the stage was animated
+			if (isBiometricDialogOpen.current === true && !isAnimated) {
+				setTimeout(authenticationProcess, 3500);
+			} else {
+				//if the stage has already been animated, the authenticationProcess Function will be executed immediately after opening the app
+				authenticationProcess();
+			}
 		}
+		let stageStatus = document.querySelector('.stage--ready');
+		stageStatus.addEventListener('animationend', () => {
+			console.log('Animation ended');
+			setIsAnimated(true);
+		});
+	};
+
+	const authenticationProcess = () => {
+		checkIdentity((error) => {
+			if (error.code === '15' || error.code === '0') {
+				isBiometricDialogOpen.current = false;
+			}
+		}, handleCheckIdentitySuccess);
 	};
 
 	const handleCheckIdentitySuccess = (credentials: Credentials) => {
@@ -69,6 +68,14 @@ export const BiometricAuthenticationLogin = () => {
 			password: credentials.password,
 			redirect: true
 		});
+
+		document
+			.querySelector('#username')
+			.setAttribute('value', credentials.username);
+		document
+			.querySelector('#passwordInput')
+			.setAttribute('value', credentials.password);
+
 		App.removeAllListeners();
 	};
 
