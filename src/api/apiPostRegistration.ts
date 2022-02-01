d4b49c7190587fe6e78e84b6ac6bd8c79543de86
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-02-01 18:29:28 +0100

    feat: [DDO-44] changed registration logic to redirect to enquiry

--- src/api/apiPostRegistration.ts
+++ src/api/apiPostRegistration.ts
@@ -1,53 +1,21 @@
-import { generateCsrfToken } from '../utils/generateCsrfToken';
 import { autoLogin } from '../components/registration/autoLogin';
-import {
-	getErrorCaseForStatus,
-	redirectToErrorPage
-} from '../components/error/errorHandling';
 import { removeAllCookies } from '../components/sessionCookie/accessSessionCookie';
+import { FETCH_ERRORS, FETCH_METHODS, fetchData } from './fetchData';
 
-let isRequestInProgress = false;
-export const apiPostRegistration = (
-	url: string,
-	data: {},
-	handleRegistrationConflictError: Function
-): Promise<any> => {
-	return new Promise((resolve, reject) => {
-		removeAllCookies();
-		if (isRequestInProgress) {
-			return reject(new Error('Request in progress'));
-		}
-		isRequestInProgress = true;
-		const xhr = new XMLHttpRequest();
-		xhr.open('POST', url);
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState > 3 && xhr.status === 201) {
-				autoLogin({
-					username: data['username'],
-					password: decodeURIComponent(data['password']),
-					redirect: false
-				})
-					.then(resolve)
-					.catch(reject);
-			} else if (xhr.readyState > 3 && xhr.status === 409) {
-				handleRegistrationConflictError(xhr);
-				isRequestInProgress = false;
-				reject(xhr);
-			} else if (
-				xhr.readyState > 3 &&
-				xhr.status !== 201 &&
-				xhr.status !== 409
-			) {
-				const error = getErrorCaseForStatus(xhr.status);
-				redirectToErrorPage(error);
-				reject(error);
-			}
-		};
-		const csrfToken = generateCsrfToken();
+export const apiPostRegistration = (url: string, data: {}): Promise<any> => {
+	removeAllCookies();
 
-		xhr.setRequestHeader('Content-Type', 'application/json');
-		xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken);
-		xhr.withCredentials = true;
-		xhr.send(JSON.stringify(data));
-	});
+	return fetchData({
+		url: url,
+		method: FETCH_METHODS.POST,
+		bodyData: JSON.stringify(data),
+		skipAuth: true,
+		responseHandling: [FETCH_ERRORS.CATCH_ALL_WITH_RESPONSE]
+	}).then(() =>
+		autoLogin({
+			username: data['username'],
+			password: decodeURIComponent(data['password']),
+			redirect: false
+		})
+	);
 };
