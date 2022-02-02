a8b74e345f940baa1792c81bf8d9ff4cbc1c6684
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-02-03 00:11:34 +0100

    feat: [COBH-4210] e2ee support check

--- src/resources/scripts/i18n/de/videoCall.ts
+++ src/resources/scripts/i18n/de/videoCall.ts
@@ -5,10 +5,20 @@ const videoCall = {
 	'button.startVideoCall': 'Videoanruf starten',
 	'button.answerVideoCall': 'Videoanruf annehmen',
 	'incomingCall.description': 'ruft an...',
+	'incomingCall.unsupported.description': 'Video-Call von %username%',
+	'incomingCall.unsupported.hint':
+		'Durch die technischen Vorraussetzungen ist der Video-Call nicht Ende-zu-Ende verschlüsselt. Jedoch ist der Video-Call transportverschlüsselt. Bitte folgen Sie der Hilfe, um Ende-zu-Ende verschlüsselt zu telefonieren.',
+	'incomingCall.unsupported.button': 'Hilfe öffnen',
 	'incomingCall.ignored': 'hat versucht Sie zu erreichen.',
 	'incomingCall.rejected.prefix': 'Sie haben versucht',
 	'incomingCall.rejected.suffix': 'zu erreichen.',
-	'incomingCall.rejected.teamconsultant.prefix': 'hat versucht'
+	'incomingCall.rejected.teamconsultant.prefix': 'hat versucht',
+	'overlay.unsupported.headline':
+		'Der Video-Call kann nicht gestartet werden',
+	'overlay.unsupported.copy':
+		'Ihr Gerät erfüllt nicht alle nötigen technischen Vorgaben für einen Video-Call. Bitte folgen Sie dieser Anleitung um telefonieren zu können. Dafür brauchen Sie möglicherweise die Unterstützung Ihrer EDV.',
+	'overlay.unsupported.button.close': 'Schließen',
+	'overlay.unsupported.button.manual': 'Zur Anleitung'
 };
 
 export default videoCall;
