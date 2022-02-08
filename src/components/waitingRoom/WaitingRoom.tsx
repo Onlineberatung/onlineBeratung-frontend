b777d290d78eec578ae6457621006d1539ac936f
Jan Amann <jan@amann.me>
2022-02-08 12:13:54 +0100

    feat: improved registration link

--- src/components/waitingRoom/WaitingRoom.tsx
+++ src/components/waitingRoom/WaitingRoom.tsx
@@ -280,19 +280,6 @@ export const WaitingRoom = (props: WaitingRoomProps) => {
 									'anonymous.waitingroom.redirect.subline'
 								)}
 							/>
-							<div className="waitingRoom__redirectButton">
-								<a href={registrationUrl}>
-									<Button
-										item={{
-											label: translate(
-												'anonymous.waitingroom.redirect.button'
-											),
-											type: 'TERTIARY'
-										}}
-										isLink={true}
-									/>
-								</a>
-							</div>
 						</div>
 					</div>
 				</>
