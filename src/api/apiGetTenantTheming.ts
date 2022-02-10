3fd98158ee62b536d5642b26fba8df20da7d69cc
Jan Amann <jan@amann.me>
2022-02-10 15:01:59 +0100

    feat: implement API call for getting tenant data

--- src/api/apiGetTenantTheming.ts
+++ src/api/apiGetTenantTheming.ts
@@ -1,9 +1,13 @@
+import { fetchData, FETCH_METHODS, FETCH_ERRORS } from './fetchData';
 import { TenantDataInterface } from '../globalState/interfaces/TenantDataInterface';
+import { config } from '../resources/scripts/config';
 
 export const apiGetTenantTheming = async (params: {
 	subdomain: string;
 }): Promise<TenantDataInterface> =>
-	// TODO: Needs to be replaced with backend call later
-	fetch(`/themes/${params.subdomain}.json`).then((response) =>
-		response.json()
-	);
+	fetchData({
+		url: config.endpoints.tenantServiceBase + '/public/' + params.subdomain,
+		method: FETCH_METHODS.GET,
+		skipAuth: true,
+		responseHandling: [FETCH_ERRORS.CATCH_ALL]
+	});
