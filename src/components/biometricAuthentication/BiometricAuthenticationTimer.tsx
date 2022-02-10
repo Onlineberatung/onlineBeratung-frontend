e1755282250c0a3b397c1be1c35805ec3f3fb094
niklasb99 <niklas.burger@virtual-identity.com>
2022-02-10 17:02:43 +0100

    refactor: using exisiting component to remove redundancies

--- src/components/biometricAuthentication/BiometricAuthenticationTimer.tsx
+++ src/components/biometricAuthentication/BiometricAuthenticationTimer.tsx
@@ -5,13 +5,17 @@ import './biometricAuthentication.styles';
 import { App } from '@capacitor/app';
 import React from 'react';
 import { isAndroid, isIOS } from 'react-device-detect';
+import { LoadingIndicator } from '../loadingIndicator/LoadingIndicator';
 
 export const BiometricAuthenticationTimer = () => {
 	let timerStart: number;
 	let timerStop: number;
-	let timerLimit = 5; //Specified in seconds
+	let timerLimit = 5; // Specified in seconds
 	const [isLogoutLoading, setIsLogoutLoading] = useState<boolean>(false);
 	const [isAppActive, setIsAppActive] = useState<boolean>(true);
+
+	// ios and Android react differently in terms of executing functions when the app is closed.
+	// Therefore, the timer is implemented differently at some points for Android and ios.
 
 	useEffect(() => {
 		document.addEventListener('visibilitychange', startAbsenceTimer);
@@ -37,6 +41,7 @@ export const BiometricAuthenticationTimer = () => {
 
 		if ((timerStop - timerStart) / 1000 > timerLimit) {
 			authProcess();
+			App.removeAllListeners();
 		}
 	};
 
@@ -54,16 +59,23 @@ export const BiometricAuthenticationTimer = () => {
 	};
 
 	const errorHandling = (error) => {
-		//ios
-		//error.code 16: Active cancellation of the authentication process by the user
-		//error.code 15: Closing the app during the authentication process
+		// This Method determines how the user cancels the authentication process
 
-		//android
-		//error.code 0: Active cancellation of the authentication process by the user
-		//error.code 0: Closing the app during the authentication process
+		// ios
+		// error.code 16: Active cancellation of the authentication process by the user
+		// error.code 15: Closing the app during the authentication process
+
+		// android
+		// error.code 0: Active cancellation of the authentication process by the user
+		// error.code 0: Closing the app during the authentication process
+
+		// If the user cancels the authentication process actively - the cancel button is pressed - the logout will take place.
+		// If the user cancels the authentication process passively - so the app is closed during the authentication process - there is still the possibility to authenticate with biometrics.
 
 		// android: no case differentiation based on the error codes is possible.
 		// In both cases the error code 0 is returned. Therefore the error message is checked
+		// Checking the error message is error prone - it depends on the language which is set on the device.
+		// Currently english and german is considered - If another language is set on the device and the user cancels the authentication process passively, the logout will take place instead.
 
 		if (isIOS) {
 			if (error.code === '15') {
@@ -78,7 +90,8 @@ export const BiometricAuthenticationTimer = () => {
 		} else if (isAndroid) {
 			if (
 				error.code === '0' &&
-				error.message === 'Verification error: Unbekannter Fehler 5'
+				(error.message === 'Verification error: Unbekannter Fehler 5' ||
+					error.message === 'Verification error: Unknown error 5')
 			) {
 				authProcessWhenRecallAppAndroid();
 			} else {
@@ -114,9 +127,8 @@ export const BiometricAuthenticationTimer = () => {
 
 	if (isLogoutLoading) {
 		return (
-			<div className="bounceEffect">
-				<div className="effect1"></div>
-				<div className="effect2"></div>
+			<div className="loadingIndicator__position">
+				<LoadingIndicator />
 			</div>
 		);
 	}
