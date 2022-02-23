8bbc979b52b76d00787c5a78505b91acc949d8eb
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-02-23 11:31:13 +0100

    fix: [COBH-4118] work

--- src/resources/scripts/i18n/de/videoCallInformal.ts
+++ src/resources/scripts/i18n/de/videoCallInformal.ts
@@ -4,9 +4,9 @@ const videoCallInformal = {
 	'incomingCall.unsupported.hint.desktop':
 		'Dein Browser unterstützt leider keine Ende-zu-Ende Verschlüsselung. Bitte wechsle zu einem der Browser <a href="https://www.google.de/chrome/" target="_blank">Google Chrome</a> oder <a href="https://www.microsoft.com/de-de/edge" target="_blank">Microsoft Edge</a>, um den Anruf annehmen zu können.',
 	'incomingCall.unsupported.hint.mobile':
-		'Dein Browser unterstützt leider keine Ende-zu-Ende Verschlüsselung. Du kannst den Anruf in der <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> annehmen oder Deinen Browser zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.cartias.de" target="_blank">Google Chrome</a> oder <a href="microsoft-edge://beratung.cartias.de/" target="_blank">Microsoft Edge</a> wechseln.',
+		'Dein Browser unterstützt leider keine Ende-zu-Ende Verschlüsselung. Du kannst den Anruf in der <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> annehmen oder Deinen Browser zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.caritas.de">Google Chrome</a> oder <a href="microsoft-edge://https%3A%2F%2Fberatung.caritas.de">Microsoft Edge</a> wechseln.',
 	'incomingCall.unsupported.hint.tablet':
-		'Dein Browser unterstützt leider keine Ende-zu-Ende Verschlüsselung. Du kannst den Anruf in der <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> annehmen oder Deinen Browser zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.cartias.de" target="_blank">Google Chrome</a> oder <a href="microsoft-edge://beratung.cartias.de/" target="_blank">Microsoft Edge</a> wechseln.',
+		'Dein Browser unterstützt leider keine Ende-zu-Ende Verschlüsselung. Du kannst den Anruf in der <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> annehmen oder Deinen Browser zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.caritas.de">Google Chrome</a> oder <a href="microsoft-edge://https%3A%2F%2Fberatung.caritas.de">Microsoft Edge</a> wechseln.',
 	'incomingCall.ignored': 'hat versucht Dich zu erreichen.',
 	'incomingCall.rejected.prefix': 'Du hast versucht',
 	'overlay.unsupported.headline':
@@ -14,9 +14,9 @@ const videoCallInformal = {
 	'overlay.unsupported.copy.browser':
 		'Bitte wechsele zu einem der Browser <a href="https://www.google.de/chrome/" target="_blank">Google Chrome</a> oder <a href="https://www.microsoft.com/de-de/edge" target="_blank">Microsoft Edge</a>, um die maximale Sicherheit zu ermöglichen und diese Funktion nutzen zu können.',
 	'overlay.unsupported.copy.mobile':
-		'Bitte wechsele zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.cartias.de" target="_blank">Google Chrome</a> oder <a href="microsoft-edge://beratung.cartias.de/" target="_blank">Microsoft Edge</a>, um die maximale Sicherheit zu ermöglichen und diese Funktion nutzen zu können.',
+		'Bitte nutze die <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> oder wechsele zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.caritas.de">Google Chrome</a> oder <a href="microsoft-edge://https%3A%2F%2Fberatung.caritas.de">Microsoft Edge</a>, um die maximale Sicherheit zu ermöglichen und diese Funktion nutzen zu können.',
 	'overlay.unsupported.copy.tablet':
-		'Bitte wechsele zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.cartias.de" target="_blank">Google Chrome</a> oder <a href="microsoft-edge://beratung.cartias.de/" target="_blank">Microsoft Edge</a>, um die maximale Sicherheit zu ermöglichen und diese Funktion nutzen zu können.'
+		'Bitte nutze die <a href="org.jitsi.meet://beratung.caritas.de">Jitsi App</a> oder wechsele zu einem der Browser <a href="googlechrome://navigate?url=https://beratung.caritas.de">Google Chrome</a> oder <a href="microsoft-edge://https%3A%2F%2Fberatung.caritas.de">Microsoft Edge</a>, um die maximale Sicherheit zu ermöglichen und diese Funktion nutzen zu können.'
 };
 
 export default videoCallInformal;
