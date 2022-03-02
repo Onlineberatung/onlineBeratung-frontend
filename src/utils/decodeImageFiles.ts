cbb8814124b949c23b1d83449b518da73b99dff6
Timo Koenig <koenig@kaiserkoenig.de>
2022-03-02 10:00:31 +0100

    fix:  more info about decoding

--- src/utils/decodeImageFiles.ts
+++ src/utils/decodeImageFiles.ts
@@ -1,4 +1,7 @@
-// decode JSON.stringified Base64 images
+/* decode JSON.stringified Base64 images with special chars
+ * due settings in Backend all base64 encoded image are delivered with  encoded spezial chars like "&#43;" for "+"
+ * here we just decode this "back to normal"
+ */
 const decode = (input: string) => {
 	return input.replace(/&#(\d+);/g, (match, dec) => {
 		return String.fromCharCode(dec);
