bb9e43e4e82b1b139ad8340aa75f23cdeb0c2f98
Alex Benz <alex@alex-benz.de>
2022-03-21 15:05:07 +0100

    feat(cobh-4246-help): added asker help content

--- src/resources/scripts/i18n/de/help.ts
+++ src/resources/scripts/i18n/de/help.ts
@@ -1,20 +1,40 @@
 const help = {
 	'googleChrome': 'Google Chrome',
 	'msEdge': 'Microsoft Edge',
-	'videoCall.headline': 'Video-Call',
-	'videoCall.intro':
-		'Um einen Video-Call starten zu können, müssen Sie sich über Google Chrome oder Microsoft Edge bei der Online-Beratung anmelden. Somit kann der Video-Call Ende-zu-Ende verschlüsselt werden und Sie können starten.',
-	'videoCall.steps': 'Schritt für Schritt Anleitung',
-	'videoCall.steps.1.1': 'Folgen Sie dem Link zu ',
-	'videoCall.steps.1.2': ' oder ',
-	'videoCall.steps.2':
-		'Laden Sie sich Chrome oder Edge herunter. Dafür brauchen Sie möglicherweise die Unterstützung Ihrer EDV.',
-	'videoCall.steps.3':
+
+	'videoCall.asker.steps.headline.1':
+		'Schritt für Schritt Anleitung: Video-Calls Ende-zu-Ende zu verschlüsseln',
+	'videoCall.asker.steps.headline.2':
+		'Sie haben bereits Google Chrome oder Microsoft Edge?',
+	'videoCall.asker.steps.1':
+		'Folgen Sie dem Link zu Google Chrome oder Microsoft Edge.',
+	'videoCall.asker.steps.2': 'Laden Sie sich Chrome oder Edge herunter.',
+	'videoCall.asker.steps.3':
 		'Installieren Sie Chrome oder Edge auf Ihrem PC/Laptop/Tablet/Smartphone.',
-	'videoCall.steps.4':
+	'videoCall.asker.steps.4':
 		'Öffnen Sie nun über Chrome oder Edge die Online-Beratung.',
-	'videoCall.steps.5': 'Melden Sie sich bei der Online-Beratung an.',
-	'videoCall.steps.6': 'Nun können Sie den Video-Call starten.',
+	'videoCall.asker.steps.5': 'Melden Sie sich bei der Online-Beratung an.',
+	'videoCall.asker.steps.6':
+		'Bitten Sie Ihre_n Berater_in Sie nochmals anzurufen.',
+
+	'videoCall.consultant.headline': 'Video-Call',
+	'videoCall.consultant.intro':
+		'Um einen Video-Call starten zu können, müssen Sie sich über Google Chrome oder Microsoft Edge bei der Online-Beratung anmelden. Somit kann der Video-Call Ende-zu-Ende verschlüsselt werden und Sie können starten.',
+	'videoCall.consultant.steps.headline.1': 'Schritt für Schritt Anleitung',
+	'videoCall.consultant.steps.headline.2':
+		'Sie haben bereits Google Chrome oder Microsoft Edge?',
+	'videoCall.consultant.steps.1.1': 'Folgen Sie dem Link zu ',
+	'videoCall.consultant.steps.1.2': ' oder ',
+	'videoCall.consultant.steps.2':
+		'Laden Sie sich Chrome oder Edge herunter. Dafür brauchen Sie möglicherweise die Unterstützung Ihrer EDV.',
+	'videoCall.consultant.steps.3':
+		'Installieren Sie Chrome oder Edge auf Ihrem PC/Laptop/Tablet/Smartphone.',
+	'videoCall.consultant.steps.4':
+		'Öffnen Sie nun über Chrome oder Edge die Online-Beratung.',
+	'videoCall.consultant.steps.5':
+		'Melden Sie sich bei der Online-Beratung an.',
+	'videoCall.consultant.steps.6': 'Nun können Sie den Video-Call starten.',
+
 	'videoCall.loginLink.title': 'Link in Zwischenablage kopieren',
 	'videoCall.loginLink.text': 'Link kopieren',
 	'videoCall.loginLink.notification.title': 'Link kopiert',
