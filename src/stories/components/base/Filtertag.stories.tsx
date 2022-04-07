a63844b4a7e34242667bc4752e3e879f01c55093
niklasb99 <niklas.burger@virtual-identity.com>
2022-04-07 19:12:02 +0200

    refactor: change svg

--- src/stories/components/base/Filtertag.stories.tsx
+++ src/stories/components/base/Filtertag.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { CloseCircle } from '../../../resources/img/icons';
 
 import {
 	Filtertag,
@@ -18,7 +19,6 @@ const Template: ComponentStory<typeof Filtertag> = (args) => (
 	<Filtertag {...args} />
 );
 
-//Strukturansicht in Storybook
 export const Default = Template.bind({});
 Default.args = {
 	size: SIZE_DEFAULT,
@@ -35,16 +35,22 @@ export const Removeable = Template.bind({});
 Removeable.args = {
 	size: SIZE_REMOVEABLE,
 	label: 'Lorem ipsum',
+	// icon: <CloseCircle />, //TODO
 	icon: (
 		<svg
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			width="16px"
-			height="16px"
-			viewBox="0 0 16 16"
 		>
-			<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0Zm3.077 4.175a.27.27 0 0 0-.383 0L8 6.87 5.306 4.175a.27.27 0 0 0-.383 0l-.844.845a.27.27 0 0 0 0 .382l2.694 2.694L4.08 10.79a.27.27 0 0 0 0 .383l.844.844a.27.27 0 0 0 .383 0L8 9.323l2.694 2.694a.27.27 0 0 0 .383 0l.844-.844a.27.27 0 0 0 0-.383L9.227 8.096l2.694-2.694a.27.27 0 0 0 0-.382Z" />
+			<path
+				d="M10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM14.3 14.3C13.91 14.69 13.28 14.69 12.89 14.3L10 11.41L7.11 14.3C6.72 14.69 6.09 14.69 5.7 14.3C5.31 13.91 5.31 13.28 5.7 12.89L8.59 10L5.7 7.11C5.31 6.72 5.31 6.09 5.7 5.7C6.09 5.31 6.72 5.31 7.11 5.7L10 8.59L12.89 5.7C13.28 5.31 13.91 5.31 14.3 5.7C14.69 6.09 14.69 6.72 14.3 7.11L11.41 10L14.3 12.89C14.68 13.27 14.68 13.91 14.3 14.3Z"
+				fill="black"
+				fill-opacity="0.87"
+			/>
 		</svg>
-	) //TODO
+	)
 };
 
 export const Readonly = Template.bind({});
