1baab3ef56e667aa3a6e05273437ce9cad798d49
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-04-26 00:32:37 +0200

    feat: [OBI-140] added error page, fixed pause

--- src/components/videoConference/WaitingRoom/Error.tsx
+++ src/components/videoConference/WaitingRoom/Error.tsx
@@ -1,13 +1,17 @@
-import { ReactComponent as ErrorIllustration } from '../../../resources/img/illustrations/ooh.svg';
+import { ReactComponent as ErrorIllustration } from '../../../resources/img/illustrations/not-found.svg';
 import { Headline } from '../../headline/Headline';
 import { translate } from '../../../utils/translate';
 import { Text } from '../../text/Text';
 import { Button, BUTTON_TYPES, ButtonItem } from '../../button/Button';
 import * as React from 'react';
 
-export const Error = () => {
+type ErrorProps = {
+	description?: string;
+};
+
+export const Error = ({ description }: ErrorProps) => {
 	const reloadButton: ButtonItem = {
-		label: translate('anonymous.waitingroom.errorPage.button'),
+		label: translate('videoConference.waitingroom.errorPage.button'),
 		type: BUTTON_TYPES.PRIMARY
 	};
 
@@ -28,12 +32,9 @@ export const Error = () => {
 						'videoConference.waitingroom.errorPage.headline'
 					)}
 				/>
-				<Text
-					type="standard"
-					text={translate(
-						'videoConference.waitingroom.errorPage.description'
-					)}
-				/>
+				{description && (
+					<Text type="standard" text={translate(description)} />
+				)}
 				<Button
 					className="waitingRoom__button"
 					buttonHandle={handleReloadButton}
--- src/resources/scripts/i18n/de/videoConference.ts
+++ src/resources/scripts/i18n/de/videoConference.ts
@@ -14,7 +14,13 @@ const videoCoference = {
 		'Der Video-Call hat noch nicht begonnen. Sie werden weitergeleitet sobald Ihr_e Berater_in den Video-Call startet.',
 	'waitingroom.paused.headline': 'Der Video-Call wurde beendet',
 	'waitingroom.paused.subline':
-		'Der Video-Call wurde beendet. Sollten Ihr_e Berater_in nur abwesend sein werden Sie in den Video-Call weitergeleitet sobald Ihr_e Berater_in den Video-Call fortsetzt.'
+		'Der Video-Call wurde beendet. Sollten Ihr_e Berater_in nur abwesend sein werden Sie in den Video-Call weitergeleitet sobald Ihr_e Berater_in den Video-Call fortsetzt.',
+	'waitingroom.errorPage.headline': 'Der Video-Call wurde nicht gefunden',
+	'waitingroom.errorPage.description':
+		'Zu Ihrem Link können wir keinen Video-Call finden da der Video-Call entweder gelöscht oder bereits beendet wurde. Sollten Sie weiterhin Probleme haben fragen Sie bitte Ihre_n Berater_in.',
+	'waitingroom.errorPage.consultant.description':
+		'Zu Ihrem Link können wir keinen Video-Call finden da der Video-Call entweder gelöscht oder bereits beendet wurde.',
+	'waitingroom.errorPage.button': 'Neu laden'
 };
 
 export default videoCoference;
