be91bf004b806d2b117da7831628e369dc365f31
niklasb99 <niklas.burger@virtual-identity.com>
2022-04-26 08:39:40 +0200

    feat: create Textinput Component

---
+++ src/components/base/Textinput.tsx
@@ -0,0 +1,331 @@
+import React, { HTMLAttributes, ReactElement, useEffect } from 'react';
+import styled from 'styled-components';
+
+interface TextinputProps extends HTMLAttributes<HTMLDivElement> {
+	inputText?: string;
+	label?: string;
+	helperText?: string;
+	password?: string;
+	lockIcon?: ReactElement;
+	eyeIcon?: ReactElement;
+	variant?: string;
+	withIcon?: boolean;
+}
+
+const StyledTextinput = styled.div`
+	${({ theme }) => `
+		font-family: ${theme.font.fontFamily};
+		font-weight: ${theme.font.fontWeight};
+		font-size: ${theme.font.fontSizeLarge};
+		line-height: ${theme.font.lineHeightLarge};
+
+		.textInput {
+			&--container {
+				display: flex;
+				align-items: center;
+			}
+
+			&--lockIcon {
+				display: none;
+				margin: ${theme.textinput.lockIcon.margin};
+
+				svg {
+					height: ${theme.textinput.lockIcon.height};
+					width: ${theme.textinput.lockIcon.width};
+				}
+			}
+
+			&--helperText {
+				display: none;
+				font-size: ${theme.font.fontSizeSmall};
+				margin: ${theme.textinput.helperText.marginWithoutIcon};
+			}
+
+			&--input {
+			
+				&-container {
+					display: inline flex;
+					align-items: center;
+					position: relative;
+
+					height:  ${theme.textinput.height};
+					width: ${theme.textinput.width};
+					padding: ${theme.textinput.padding};
+
+					border: ${theme.textinput.border.style};
+					border-radius: ${theme.textinput.border.radius};
+					box-shadow: ${theme.textinput.border.boxShadow} ${theme.colors.shadow};
+					box-sizing: ${theme.textinput.border.boxSizing};
+				}
+
+				&-label {
+					font-size: ${theme.font.fontSizeSmall};
+					line-height: ${theme.font.lineHeightSmall};
+					color: ${theme.colors.label};
+				}
+
+				&-text {
+					all: ${theme.textinput.inputText.styleReset};
+					width: ${theme.textinput.inputText.widthWithoutIcon};
+					overflow: ${theme.textinput.inputText.overflow};
+					&::placeholder {
+						color: ${theme.colors.black};
+					}
+				}
+
+				&-eyeIcon {
+					display: none;
+					position: ${theme.textinput.eyeIcon.position};
+					right: ${theme.textinput.eyeIcon.right};
+
+					svg {
+						height: ${theme.textinput.eyeIcon.height};
+						width: ${theme.textinput.eyeIcon.width};
+
+						path {
+							fill: ${theme.colors.default}
+						}
+					}
+				}
+			}
+		}
+
+		&.default {
+			.textInput--input {
+				&-label {
+					display: none;
+				}
+
+				&-container {
+					border-color: ${theme.colors.default};
+				}
+
+				&-text::placeholder {
+					color: ${theme.colors.default};
+				}
+			}
+		}
+
+		&.selected {
+			.textInput--input-container {
+				border: ${theme.textinput.border.styleBold} ${theme.colors.black};
+			}
+		}
+
+		&.activated {
+			.textInput--input-container {
+				border: ${theme.textinput.border.style} ${theme.colors.black};
+			}
+		}
+
+		&.success {
+			.textInput {
+				&--helperText {
+					display: block;
+					color: ${theme.colors.success};
+				}
+
+				&--input-container {
+					border: ${theme.textinput.border.styleBold} ${theme.colors.success};
+				}
+			}
+		}
+
+		&.warning {
+			.textInput {
+				&--helperText {
+					display: block;
+					color: ${theme.colors.warning};
+				}
+
+				&--input-container {
+					border: ${theme.textinput.border.styleBold} ${theme.colors.warning};
+				}
+			}
+		}
+
+		&.error {
+			.textInput {
+				&--helperText {
+					display: block;
+					color: ${theme.colors.error};
+				}
+
+				&--input-container {
+					border: ${theme.textinput.border.styleBold} ${theme.colors.error};
+				}
+			}
+		}
+
+		&.disabled {
+			.textInput--input {
+				&-eyeIcon {
+					svg {
+						path {
+							fill: ${theme.colors.black}
+						}
+					}
+				}
+
+				&-container {
+					border-color: ${theme.colors.disabled};
+					box-shadow: none;
+
+					input::placeholder {
+						color: ${theme.colors.disabled};
+					}
+				}
+
+				&-label {
+					display: none;
+				}
+			}
+		}
+
+		&.withIcon {
+			.textInput {
+				&--input-eyeIcon {
+					display: block;
+				}
+
+				&--input-text {
+					width: ${theme.textinput.inputText.widthWithIcon};
+				}
+				
+				&--lockIcon {
+					display: inline;
+				}
+			
+				&--helperText {
+					margin: ${theme.textinput.helperText.marginWithIcon};
+				}
+			}
+		}
+	`}
+`;
+
+StyledTextinput.defaultProps = {
+	theme: {
+		colors: {
+			default: '#00000066',
+			success: '#4FCC5C',
+			warning: '#FF9F00',
+			error: '#FF0000',
+			disabled: '#00000033',
+			black: '#000000DE',
+			shadow: '#0000001A',
+			label: '#00000099'
+		},
+
+		font: {
+			fontFamily: 'Roboto, sans-serif',
+			fontWeight: '400',
+			fontSizeLarge: '16px',
+			fontSizeSmall: '12px',
+			lineHeightLarge: '150%',
+			lineHeightSmall: '133%'
+		},
+
+		textinput: {
+			height: '48px',
+			width: '197px',
+			padding: '0px 12px 0px 16px',
+
+			border: {
+				radius: '4px',
+				style: '1px solid',
+				styleBold: '2px solid',
+				boxShadow: 'inset 0px 2px 0px 1px',
+				boxSizing: 'border-box'
+			},
+
+			lockIcon: {
+				margin: '0 12px 0 0',
+				height: '21px',
+				width: '16px'
+			},
+
+			eyeIcon: {
+				position: 'absolute',
+				right: '12.83px',
+				height: '12.5px',
+				width: '18.33px'
+			},
+
+			helperText: {
+				marginWithoutIcon: '0 0 0 19px',
+				marginWithIcon: '0 0 0 45px'
+			},
+
+			inputText: {
+				styleReset: 'unset',
+				widthWithoutIcon: '165px',
+				widthWithIcon: '140px',
+				overflow: 'hidden'
+			}
+		}
+	}
+};
+
+export const Textinput = ({
+	inputText,
+	label,
+	helperText,
+	password = 'text',
+	lockIcon,
+	eyeIcon,
+	variant = 'default',
+	withIcon = true,
+	className,
+	...props
+}: TextinputProps) => {
+	useEffect(() => {
+		let input = document.getElementsByClassName(
+			'textInput--input-text'
+		)[0] as HTMLInputElement;
+
+		if (variant == 'disabled') {
+			input.setAttribute('disabled', 'true');
+		} else {
+			input.removeAttribute('disabled');
+		}
+	}, [variant]);
+
+	return (
+		<StyledTextinput
+			type="textinput"
+			className={`${className} ${variant} ${withIcon && 'withIcon'} `}
+			{...props}
+		>
+			<div className="textInput--container">
+				<div className="textInput--lockIcon">
+					{' '}
+					{lockIcon && lockIcon}{' '}
+				</div>
+
+				<div className="textInput--input-container">
+					<div>
+						<div className="textInput--input-label">
+							{' '}
+							{label && label}{' '}
+						</div>
+						<input
+							type={password && password}
+							className="textInput--input-text"
+							placeholder={inputText && inputText}
+						/>
+					</div>
+					<div className="textInput--input-eyeIcon">
+						{' '}
+						{eyeIcon && eyeIcon}{' '}
+					</div>
+				</div>
+			</div>
+
+			<span className="textInput--helperText">
+				{' '}
+				{helperText && helperText}{' '}
+			</span>
+		</StyledTextinput>
+	);
+};
---
+++ src/stories/components/base/Textinput.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Textinput } from '../../../components/base/Textinput';
+
+export default {
+	title: 'Base/Textinput',
+	component: Textinput,
+	argTypes: {
+		password: { options: ['text', 'password'], control: { type: 'radio' } },
+		variant: {
+			options: [
+				'disabled',
+				'default',
+				'selected',
+				'activated',
+				'success',
+				'warning',
+				'error'
+			],
+			control: { type: 'radio' }
+		},
+		withIcon: { control: 'boolean' },
+		eyeIcon: { table: { disable: true } },
+		lockIcon: { table: { disable: true } }
+	}
+} as ComponentMeta<typeof Textinput>;
+
+const Template: ComponentStory<typeof Textinput> = (args) => (
+	<Textinput {...args} />
+);
+
+export const Active = Template.bind({});
+Active.args = {
+	inputText: 'Placeholder Text',
+	label: 'Label',
+	helperText: 'Helper text',
+	lockIcon: (
+		<svg
+			width="16"
+			height="21"
+			viewBox="0 0 16 21"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M14 7H13V5C13 2.24 10.76 0 8 0C5.24 0 3 2.24 3 5V7H2C0.9 7 0 7.9 0 9V19C0 20.1 0.9 21 2 21H14C15.1 21 16 20.1 16 19V9C16 7.9 15.1 7 14 7ZM8 16C6.9 16 6 15.1 6 14C6 12.9 6.9 12 8 12C9.1 12 10 12.9 10 14C10 15.1 9.1 16 8 16ZM5 7V5C5 3.34 6.34 2 8 2C9.66 2 11 3.34 11 5V7H5Z"
+				fill="black"
+				fill-opacity="0.87"
+			/>
+		</svg>
+	),
+	eyeIcon: (
+		<svg
+			width="22"
+			height="15"
+			viewBox="0 0 22 15"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M11 0C6 0 1.73 3.11 0 7.5C1.73 11.89 6 15 11 15C16 15 20.27 11.89 22 7.5C20.27 3.11 16 0 11 0ZM11 12.5C8.24 12.5 6 10.26 6 7.5C6 4.74 8.24 2.5 11 2.5C13.76 2.5 16 4.74 16 7.5C16 10.26 13.76 12.5 11 12.5ZM11 4.5C9.34 4.5 8 5.84 8 7.5C8 9.16 9.34 10.5 11 10.5C12.66 10.5 14 9.16 14 7.5C14 5.84 12.66 4.5 11 4.5Z"
+				fill="black"
+				fill-opacity="0.87"
+			/>
+		</svg>
+	)
+};
