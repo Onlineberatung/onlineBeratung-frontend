1e7bf2c518248c0ce323d5453bb9b0e6fb5586c3
niklasb99 <niklas.burger@virtual-identity.com>
2022-05-10 10:38:50 +0200

    refactor: optimize Accordeon component

--- src/components/base/Accordeon.tsx
+++ src/components/base/Accordeon.tsx
@@ -1,23 +1,35 @@
-import React, { HTMLAttributes, ReactElement, useEffect, useRef } from 'react';
+import React, {
+	HTMLAttributes,
+	ReactElement,
+	useEffect,
+	useRef,
+	useState
+} from 'react';
 import styled from 'styled-components';
 
 interface AccordeonProps extends HTMLAttributes<HTMLDivElement> {
 	label?: string;
-	icon?: ReactElement;
+	iconCheckmark?: ReactElement;
+	iconDown?: ReactElement;
+	iconUp?: ReactElement;
 	withIcon?: boolean;
 }
 
 const StyledAccordeon = styled.div`
 	${({ theme }) => `
+		font-family: ${theme.font.family};
+		font-weight: ${theme.font.weight};
+		font-size: ${theme.font.size};
+		line-height: ${theme.font.lineHeight};
+		
+        color: ${theme.colors.darkGrey};
+
+        border-bottom: ${theme.border.style} ${theme.colors.lightGrey};
+		
         display: flex;
         justify-content: space-between;
         flex-wrap: wrap;
-        font-family: ${theme.accordeon.fontFamily};
-        font-weight: ${theme.accordeon.fontWeight};
-        font-size: ${theme.accordeon.fontSize};
-        line-height: ${theme.accordeon.lineHeight};
-        color: ${theme.colors.darkGrey};
-        border-bottom: ${theme.accordeon.borderBottom} ${theme.colors.lightGrey};
+		
         width: ${theme.accordeon.width};
 
         &:hover {
@@ -29,7 +41,7 @@ const StyledAccordeon = styled.div`
         }
 
         .label {
-            padding-bottom: ${theme.accordeon.paddingBottom};
+            padding: ${theme.accordeon.padding};
         }
 
         .icon--container {
@@ -77,21 +89,30 @@ StyledAccordeon.defaultProps = {
 			hover: '#A31816',
 			green: '#4FCC5C'
 		},
+
+		font: {
+			family: 'Roboto, sans-serif',
+			weight: '700',
+			size: '16px',
+			lineHeight: '131%'
+		},
+
+		border: {
+			style: '1px solid'
+		},
+
 		accordeon: {
-			fontFamily: 'Roboto, sans-serif',
-			fontWeight: '700',
-			fontSize: '16px',
-			lineHeight: '131%',
-			borderBottom: '1px solid',
 			width: '500px',
-			paddingBottom: '16px'
+			padding: '0 0 16px 0'
 		}
 	}
 };
 
 export const Accordeon = ({
 	label,
-	icon,
+	iconCheckmark,
+	iconDown,
+	iconUp,
 	withIcon = false,
 	className,
 	...props
@@ -101,19 +122,19 @@ export const Accordeon = ({
 	const accordeonContentRef = useRef(null);
 	const iconRef = useRef(null);
 
-	let contentVisible = false;
+	const [isExpanded, setIsExpanded] = useState(false);
 
 	const expandAccordeon = () => {
-		if (!contentVisible) {
+		if (isExpanded == false) {
 			arrowDownRef.current.style.display = 'none';
-			arrowUpRef.current.style.display = 'inline';
+			arrowUpRef.current.style.display = 'block';
 			accordeonContentRef.current.style.display = 'block';
 		} else {
 			arrowDownRef.current.style.display = 'inline';
 			arrowUpRef.current.style.display = 'none';
 			accordeonContentRef.current.style.display = 'none';
 		}
-		contentVisible = !contentVisible;
+		setIsExpanded(!isExpanded);
 	};
 
 	useEffect(() => {
@@ -132,39 +153,13 @@ export const Accordeon = ({
 			<span className="label">{label && label}</span>
 
 			<div className="icon--container">
-				<span ref={iconRef}>{icon && icon}</span>
-				<svg
-					id="arrow-down"
-					className="arrow"
-					ref={arrowDownRef}
-					width="20"
-					height="12"
-					viewBox="0 0 20 12"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M16.9351 0.52349L9.99105 7.46756L3.04698 0.52349C2.34899 -0.174497 1.22148 -0.174497 0.52349 0.52349C-0.174497 1.22148 -0.174497 2.34899 0.52349 3.04698L8.73826 11.2617C9.43624 11.9597 10.5638 11.9597 11.2617 11.2617L19.4765 3.04698C20.1745 2.34899 20.1745 1.22148 19.4765 0.52349C18.7785 -0.1566 17.6331 -0.174497 16.9351 0.52349Z"
-						fill="black"
-						fill-opacity="0.87"
-					/>
-				</svg>
-				<svg
-					id="arrow-up"
-					className="arrow arrow--up"
-					ref={arrowUpRef}
-					width="20"
-					height="12"
-					viewBox="0 0 20 12"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M8.73825 0.52349L0.52349 8.73825C-0.174497 9.43624 -0.174497 10.5638 0.52349 11.2617C1.22148 11.9597 2.34899 11.9597 3.04698 11.2617L10.0089 4.31767L16.953 11.2617C17.651 11.9597 18.7785 11.9597 19.4765 11.2617C20.1745 10.5638 20.1745 9.43624 19.4765 8.73825L11.2617 0.52349C10.5817 -0.174497 9.43624 -0.174497 8.73825 0.52349Z"
-						fill="black"
-						fill-opacity="0.87"
-					/>
-				</svg>
+				<span ref={iconRef}>{iconCheckmark && iconCheckmark}</span>
+				<span className="arrow arrow--down" ref={arrowDownRef}>
+					{iconDown && iconDown}
+				</span>
+				<span className="arrow arrow--up" ref={arrowUpRef}>
+					{iconUp && iconUp}
+				</span>
 			</div>
 
 			<div className="accordeon--content" ref={accordeonContentRef}></div>
