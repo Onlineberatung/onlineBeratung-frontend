69efd4cf31584cf56fbbce418472b7da64698e6f
Franz Benthin <franz.benthin@altow.de>
2022-05-18 15:05:22 +0200

    feat: [OBI-267] add DragAndDropArea component and styles

---
+++ src/components/dragAndDropArea/DragAndDropArea.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useState } from 'react';
+import './dragAndDropArea.styles';
+import { useDropzone } from 'react-dropzone';
+import clsx from 'clsx';
+import { ReactComponent as UploadIcon } from '../../resources/img/icons/upload.svg';
+import { translate } from '../../utils/translate';
+
+interface DragAndDropAreaProps {
+	onFileDragged: (file: File) => void;
+	isDragging: boolean;
+	canDrop: boolean;
+	onDragLeave: () => void;
+	styleOverride?: React.CSSProperties;
+}
+
+export const DragAndDropArea = (props: DragAndDropAreaProps) => {
+	const [canDrop, setCanDrop] = useState(false);
+	const onDrop = useCallback(
+		(acceptedFiles) => props.onFileDragged(acceptedFiles[0]),
+		[props]
+	);
+	const onDragEnter = () => setCanDrop(true);
+	const onDragLeave = () => {
+		setCanDrop(false);
+		props.onDragLeave();
+	};
+
+	const { getRootProps, getInputProps } = useDropzone({
+		onDrop,
+		noClick: true,
+		onDragEnter,
+		onDragLeave
+	});
+
+	return (
+		<div
+			className={clsx('dragAndDropArea', props.isDragging && 'enabled')}
+			style={props.styleOverride}
+			{...getRootProps()}
+		>
+			<div className={clsx('dropContainer', canDrop && 'can-drop')}>
+				<input {...getInputProps()} />
+				<UploadIcon className="dropContainer__icon" />
+				<div className="dropContainer__explanation">
+					{translate(
+						canDrop
+							? 'session.dragAndDrop.explanation.insideDropArea'
+							: 'session.dragAndDrop.explanation.outsideDropArea'
+					)}
+				</div>
+				<div className="dropContainer__restrictions">
+					{translate('session.dragAndDrop.restrictions')}
+				</div>
+			</div>
+		</div>
+	);
+};
