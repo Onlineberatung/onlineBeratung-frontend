47097ff423b8273d6ed68c2178c4dd6d92d447d7
Franz Benthin <franz.benthin.dev@gmail.com>
2022-07-07 10:07:44 +0200

    fix: [OBI-589] change ternary operation order for readability increase

--- src/api/apiRocketChatSettings.ts
+++ src/api/apiRocketChatSettings.ts
@@ -6,8 +6,8 @@ export const apiRocketChatSettings = async (
 	settingsEntries: string[] = null
 ): Promise<any> => {
 	const url = config.endpoints.rc.settings;
-	const query = !settingsEntries
-		? ''
-		: '?query=' + JSON.stringify({ _id: { $in: settingsEntries } });
+	const query = settingsEntries
+		? '?query=' + JSON.stringify({ _id: { $in: settingsEntries } })
+		: '';
 	return fetchRCData(url + query, FETCH_METHODS.GET);
 };
