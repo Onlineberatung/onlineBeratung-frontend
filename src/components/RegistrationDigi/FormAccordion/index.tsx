90fd8a49d09d0a501684a3179d3ec9e0c67cb0f9
Carlos Soares <carlos.soares17@gmail.com>
2022-08-03 07:02:11 +0100

    feat: adding the improvements

--- src/components/RegistrationDigi/FormAccordion/index.tsx
+++ src/components/RegistrationDigi/FormAccordion/index.tsx
@@ -1,13 +1,19 @@
 import { FieldContext } from 'rc-field-form';
+import { FormInstance } from 'rc-field-form';
 import React, { useCallback, useState, Children } from 'react';
 import { InvalidIcon } from '../../../resources/img/icons';
+import { translate } from '../../../utils/translate';
+import { Button, ButtonItem, BUTTON_TYPES } from '../../button/Button';
 import './formAccordion.styles.scss';
 
 interface FormAccordionProps {
 	children: React.ReactChild | React.ReactChild[];
+	onComplete?: () => void;
 }
 
 interface FormAccordionItemProps {
+	disableNextButton?: boolean;
+	form?: FormInstance;
 	stepNumber?: number;
 	title: string;
 	subTitle?: string;
@@ -16,8 +22,17 @@ interface FormAccordionItemProps {
 	children: React.ReactChild | React.ReactChild[];
 }
 
-export const FormAccordion = ({ children }: FormAccordionProps) => {
-	const [activePanel, setActivePanel] = useState(null);
+export const FormAccordion = ({ children, onComplete }: FormAccordionProps) => {
+	const [activePanel, setActivePanel] = useState(0);
+
+	const onClickNext = useCallback(() => {
+		if (activePanel < Children.count(children) - 1) {
+			setActivePanel(activePanel + 1);
+		} else {
+			setActivePanel(null);
+			onComplete?.();
+		}
+	}, [activePanel, children, onComplete]);
 
 	const handlePanelClick = useCallback(
 		(panel: number) => {
@@ -29,8 +44,12 @@ export const FormAccordion = ({ children }: FormAccordionProps) => {
 	return (
 		<div className="formAccordionDigi">
 			{Children.toArray(children).map((child, index) => {
-				if ((child as JSX.Element).type.name !== 'FormAccordionItem') {
-					console.error('FormAccordionItem expected');
+				if (
+					(child as JSX.Element).type.displayName !==
+						'FormAccordionItem' &&
+					process.env.NODE_ENV === 'development'
+				) {
+					console.warn('FormAccordionItem expected');
 				}
 
 				return (
@@ -38,6 +57,7 @@ export const FormAccordion = ({ children }: FormAccordionProps) => {
 						key={index}
 						index={index}
 						handlePanelClick={handlePanelClick}
+						handleNextStep={onClickNext}
 						isActive={activePanel === index}
 						{...((child as JSX.Element)
 							.props as FormAccordionItemProps)}
@@ -47,6 +67,7 @@ export const FormAccordion = ({ children }: FormAccordionProps) => {
 		</div>
 	);
 };
+FormAccordion.displayName = 'FormAccordion';
 
 export const FormAccordionPanel = ({
 	stepNumber,
@@ -56,21 +77,46 @@ export const FormAccordionPanel = ({
 	errorOnTouchExtraFields = [],
 	children,
 	handlePanelClick,
+	handleNextStep,
 	isActive,
-	index
+	index,
+	form,
+	disableNextButton
 }: FormAccordionItemProps & {
 	index: number;
 	isActive: boolean;
 	handlePanelClick: (index: number) => void;
+	handleNextStep: () => void;
 }) => {
 	const formContext = React.useContext(FieldContext);
 	const fieldsToCheck = [...formFields, ...errorOnTouchExtraFields];
+	const isFieldsInValid = (form || formContext)
+		.getFieldsError(formFields)
+		.some((error) => error.errors.length !== 0);
+
 	const isValid = !(
-		formContext.isFieldsTouched(fieldsToCheck) &&
-		formContext
-			.getFieldsError(formFields)
-			.some((error) => error.errors.length !== 0)
+		formContext.isFieldsTouched(fieldsToCheck) && isFieldsInValid
 	);
+
+	const buttonAnswerVideoCall: ButtonItem = {
+		title: translate('registration.accordion.item.continueButton.title'),
+		label: translate('registration.accordion.item.continueButton'),
+		type: BUTTON_TYPES.LINK
+	};
+
+	const childrenWithProps = Children.map(children, (child) => {
+		// With this code we can have multi accordion levels and have a next button
+		if ((child as JSX.Element)?.type?.displayName === 'FormAccordion') {
+			return (
+				child &&
+				React.cloneElement(child as React.ReactElement, {
+					onComplete: handleNextStep
+				})
+			);
+		} else {
+			return child;
+		}
+	});
 
 	return (
 		<div className={`formAccordionDigi__Panel ${isActive ? 'active' : ''}`}>
@@ -98,14 +144,26 @@ export const FormAccordionPanel = ({
 					isActive ? 'active' : ''
 				}`}
 			>
-				{children}
+				{childrenWithProps}
+
+				{!disableNextButton && (
+					<Button
+						buttonHandle={handleNextStep}
+						item={buttonAnswerVideoCall}
+						disabled={isFieldsInValid}
+						className="formAccordionItem__continueButton"
+						tabIndex={-1}
+					/>
+				)}
 			</div>
 		</div>
 	);
 };
 
-export const FormAccordionItem = ({ children }: FormAccordionItemProps) => {
-	return <div className="FormAccordionBody">{children}</div>;
-};
+export const FormAccordionItem = ({ children }: FormAccordionItemProps) => (
+	<div className="FormAccordionBody">{children}</div>
+);
+
+FormAccordionItem.displayName = 'FormAccordionItem';
 
 FormAccordion.Item = FormAccordionItem;
