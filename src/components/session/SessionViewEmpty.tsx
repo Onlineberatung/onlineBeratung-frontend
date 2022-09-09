8fdfa87650cb8263d7a276e4e954128c0280dcab
Andre <andre.soares@virtual-identity.com>
2022-09-09 08:51:16 +0100

    fix: try budibase login on sessionView

--- src/components/session/SessionViewEmpty.tsx
+++ src/components/session/SessionViewEmpty.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import { TenantContext } from '../../globalState';
+import { config } from '../../resources/scripts/config';
 import { translate } from '../../utils/translate';
 import { mobileListView } from '../app/navigationHandler';
 import { Text } from '../text/Text';
 import './session.styles';
 
 export const SessionViewEmpty = () => {
+	const { tenant } = useContext(TenantContext);
+
 	useEffect(() => {
 		mobileListView();
+		const ifrm = document.createElement('iframe');
+		ifrm.setAttribute(
+			'src',
+			`${config.urls.budibaseDevServer}/api/global/auth/default/oidc/configs/${tenant?.settings?.featureToolsOICDToken}`
+		);
+		ifrm.id = 'authIframe2';
+		ifrm.style.display = 'none';
+		document.body.appendChild(ifrm);
 	}, []);
 	return (
 		<div className="session session--empty">
