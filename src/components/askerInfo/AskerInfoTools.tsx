0fbecd0dfa591681f8018972018ddc42b0af161b
Andre <andre.soares@virtual-identity.com>
2022-09-20 14:27:03 +0100

    fix: fix shared tool link

--- src/components/askerInfo/AskerInfoTools.tsx
+++ src/components/askerInfo/AskerInfoTools.tsx
@@ -19,7 +19,7 @@ export const AskerInfoTools = () => {
 		window.open(
 			`${config.endpoints.budibaseTools(
 				activeSession.consultant.id
-			)}/roleSync?userId=${askerId}&access_token=${accessToken}`,
+			)}/consultant_view?userId=${askerId}&access_token=${accessToken}`,
 			'_blank',
 			'noopener'
 		);
