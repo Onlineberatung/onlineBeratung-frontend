08a55e27a725f25df291ac874455c5ef0fc35168
Melanie Jakob <mja@altow.de>
2022-09-23 08:06:46 +0200

    feat: [OBI-751] add language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n, { InitOptions } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import _ from 'lodash';
 
 import { de } from './resources/i18n/de';
@@ -33,24 +34,32 @@ const resources = {
 };
 
 export const init = (config: InitOptions) => {
-	return i18n.use(initReactI18next).init(
-		_.merge(
-			{
-				defaultNS: 'common',
-				fallbackLng: {
-					de_informal: ['de'],
-					default: [FALLBACK_LNG]
+	return i18n
+		.use(LanguageDetector)
+		.use(initReactI18next)
+		.init(
+			_.merge(
+				{
+					detection: {
+						order: ['sessionStorage', 'navigator'],
+						lookupQuerystring: 'lng',
+						lookupSessionStorage: 'locale',
+						caches: ['sessionStorage']
+					},
+					defaultNS: 'common',
+					fallbackLng: {
+						de_informal: ['de'],
+						default: [FALLBACK_LNG]
+					},
+					returnEmptyString: true,
+					interpolation: {
+						escapeValue: false
+					},
+					resources
 				},
-				lng: 'de',
-				returnEmptyString: true,
-				interpolation: {
-					escapeValue: false
-				},
-				resources
-			},
-			config ?? {}
-		)
-	);
+				config ?? {}
+			)
+		);
 };
 
 export default i18n;
