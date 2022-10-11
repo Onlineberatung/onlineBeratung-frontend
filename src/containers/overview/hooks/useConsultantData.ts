c7d9dd2a8dbc9c2919ae335fd011c45d68bc8f10
Carlos Soares <carlos.soares17@gmail.com>
2022-10-11 10:08:17 +0100

    fix: only show unread messages

--- src/containers/overview/hooks/useConsultantData.ts
+++ src/containers/overview/hooks/useConsultantData.ts
@@ -5,8 +5,12 @@ import { ListItemsResponseInterface } from '../../../globalState/interfaces/Sess
 
 interface ConsultantDataProps {
 	type: SESSION_LIST_TYPES;
+	unReadOnly?: boolean;
 }
-export const useConsultantData = ({ type }: ConsultantDataProps) => {
+export const useConsultantData = ({
+	type,
+	unReadOnly
+}: ConsultantDataProps) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [data, setData] = useState<ListItemsResponseInterface>({
 		count: 0,
@@ -21,14 +25,29 @@ export const useConsultantData = ({ type }: ConsultantDataProps) => {
 
 		apiGetConsultantSessionList({
 			type,
-			count: 10,
+			count: 50,
 			signal: abortController.signal
 		})
-			.then(setData)
+			.then((data) => {
+				if (unReadOnly) {
+					// Since the backend doesn't support this filter we need to grab more messages
+					// to filter by 9
+					const sessions = data.sessions.filter(
+						(session) => !session.session.messagesRead
+					);
+					setData({
+						...data,
+						total: sessions.length,
+						sessions
+					});
+				} else {
+					setData(data);
+				}
+			})
 			.finally(() => setIsLoading(false));
 
 		return () => abortController.abort();
-	}, [type]);
+	}, [type, unReadOnly]);
 
 	return { ...data, isLoading };
 };
