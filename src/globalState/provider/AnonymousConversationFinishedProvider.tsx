a5989e8e21885a58654623be142a9260c88cd4e0
Marc Itzenthaler <itzenthaler@web-mi.de>
2022-11-09 23:28:17 +0100

    fix: live chat finish overlay

--- src/globalState/provider/AnonymousConversationFinishedProvider.tsx
+++ src/globalState/provider/AnonymousConversationFinishedProvider.tsx
@@ -2,22 +2,29 @@ import * as React from 'react';
 import { createContext, useCallback, useContext, useState } from 'react';
 import { UserDataContext } from './UserDataProvider';
 import { AUTHORITIES, hasUserAuthority } from '../helpers/stateHelpers';
+import useUpdatingRef from '../../hooks/useUpdatingRef';
 
 export const AnonymousConversationFinishedContext = createContext<any>(null);
 
 export function AnonymousConversationFinishedProvider(props) {
 	const { userData } = useContext(UserDataContext);
+	const userDataRef = useUpdatingRef(userData);
 	const [anonymousConversationFinished, setAnonymousConversationFinished] =
 		useState<string>(null);
 
 	// Temporary fix until refactoring because this state is only relevant for RS.
 	const handleAnonymousConversationFinished = useCallback(
 		(state) => {
-			if (hasUserAuthority(AUTHORITIES.ANONYMOUS_DEFAULT, userData)) {
+			if (
+				hasUserAuthority(
+					AUTHORITIES.ANONYMOUS_DEFAULT,
+					userDataRef.current
+				)
+			) {
 				setAnonymousConversationFinished(state);
 			}
 		},
-		[userData]
+		[userDataRef]
 	);
 
 	return (
