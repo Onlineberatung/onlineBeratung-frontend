b1c8351f6706eeb566890781c09c81443507c05d
Carlos Soares <carlos.soares17@gmail.com>
2022-12-09 11:49:23 +0000

    fix: trying to fix registration when autoselect is enabled

--- src/components/consultingTypeSelection/ConsultingTypeAgencySelection.tsx
+++ src/components/consultingTypeSelection/ConsultingTypeAgencySelection.tsx
@@ -263,22 +263,25 @@ const AgencySelection = ({
 					key={agency.id}
 					className="agencySelection__proposedAgency"
 				>
-					<RadioButton
-						name="agencySelection"
-						handleRadioButton={() => onChange(agency)}
-						type="default"
-						value={agency.id.toString()}
-						checked={
-							selectedAgency && agency.id === selectedAgency.id
-						}
-						inputId={agency.id.toString()}
-						label={translate(
-							[`agency.${agency.id}.name`, agency.name],
-							{ ns: 'agencies' }
-						)}
-						onKeyDown={onKeyDown}
-					/>
-					<AgencyInfo agency={agency} />
+					<div className="agencySelection__proposedAgency__container">
+						<RadioButton
+							name="agencySelection"
+							handleRadioButton={() => onChange(agency)}
+							type="default"
+							value={agency.id.toString()}
+							checked={
+								selectedAgency &&
+								agency.id === selectedAgency.id
+							}
+							inputId={agency.id.toString()}
+							label={translate(
+								[`agency.${agency.id}.name`, agency.name],
+								{ ns: 'agencies' }
+							)}
+							onKeyDown={onKeyDown}
+						/>
+						<AgencyInfo agency={agency} />
+					</div>
 					<AgencyLanguages agencyId={agency.id} />
 				</div>
 			))}
