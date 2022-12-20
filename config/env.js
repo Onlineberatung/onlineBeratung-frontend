dc4d7c889c37edbe976a9008310eb50b00685616
Rebecca Emmanuel <rebeccaemmanuel1995@gmail.com>
2022-12-20 13:11:52 +0100

    feat(branch): add branch env to js scripts

--- config/env.js
+++ config/env.js
@@ -87,7 +87,8 @@ function getClientEnvironment(publicUrl) {
 				WDS_SOCKET_PORT: process.env.WDS_SOCKET_PORT,
 				// Whether or not react-refresh is enabled.
 				// It is defined here so it is available in the webpackHotDevClient.
-				FAST_REFRESH: process.env.FAST_REFRESH !== 'false'
+				FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
+				BRANCH_NAME: process.env.BRANCH_NAME
 			}
 		);
 	// Stringify all values so we can feed into webpack DefinePlugin
--- scripts/start.js
+++ scripts/start.js
@@ -9,6 +9,7 @@ process.env.REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY =
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
+process.env.BRANCH_NAME = 'develop';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
