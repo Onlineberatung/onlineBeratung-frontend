a09e17fcdcf51c120e03323ac8332fbeaf2c5c06
Rebecca Emmanuel <rebeccaemmanuel1995@gmail.com>
2023-02-17 08:34:03 +0100

    fix(stop video call): return type

--- src/api/apiStopVideoCall.ts
+++ src/api/apiStopVideoCall.ts
@@ -1,9 +1,7 @@
 import { endpoints } from '../resources/scripts/endpoints';
 import { fetchData, FETCH_METHODS, FETCH_SUCCESS } from './fetchData';
 
-export const apiStopVideoCall = async (
-	roomId: string
-): Promise<{ moderatorVideoCallUrl: string }> => {
+export const apiStopVideoCall = async (roomId: string): Promise<void> => {
 	const url = endpoints.stopVideoCall + '/' + roomId;
 
 	return fetchData({
