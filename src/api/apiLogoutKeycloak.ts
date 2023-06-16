c3537c0eb6658a36b8bdbc540c39eed83b14ae2a
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-06-16 19:57:51 +0200

    fix: double redirect if logout fails

--- src/api/apiLogoutKeycloak.ts
+++ src/api/apiLogoutKeycloak.ts
@@ -1,17 +1,13 @@
 import { endpoints } from '../resources/scripts/endpoints';
 import { getValueFromCookie } from '../components/sessionCookie/accessSessionCookie';
-import {
-	getErrorCaseForStatus,
-	redirectToErrorPage
-} from '../components/error/errorHandling';
 
-export const apiKeycloakLogout = (): Promise<any> =>
-	new Promise((resolve, reject) => {
-		const url = endpoints.keycloakLogout;
-		const refreshToken = getValueFromCookie('refreshToken');
-		const data = `client_id=app&grant_type=refresh_token&refresh_token=${refreshToken}`;
+export const apiKeycloakLogout = async (): Promise<any> => {
+	const url = endpoints.keycloakLogout;
+	const refreshToken = getValueFromCookie('refreshToken');
+	const data = `client_id=app&grant_type=refresh_token&refresh_token=${refreshToken}`;
 
-		const req = new Request(url, {
+	return fetch(
+		new Request(url, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
@@ -19,19 +15,6 @@ export const apiKeycloakLogout = (): Promise<any> =>
 			},
 			credentials: 'include',
 			body: data
-		});
-
-		fetch(req)
-			.then((response) => {
-				if (response.status === 204) {
-					resolve(response);
-				} else {
-					const error = getErrorCaseForStatus(response.status);
-					redirectToErrorPage(error);
-					reject(new Error('keycloakLogout'));
-				}
-			})
-			.catch((error) => {
-				reject(error);
-			});
-	});
+		})
+	);
+};
