cff4990836627f59396626b5e371886057074584
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-08-03 16:46:50 +0200

    fix: allow join video call with invalid/expired token as anonymous

--- src/api/appointments/getAppointment.ts
+++ src/api/appointments/getAppointment.ts
@@ -3,13 +3,15 @@ import { endpoints } from '../../resources/scripts/endpoints';
 import { FETCH_ERRORS, FETCH_METHODS, fetchData } from '../fetchData';
 
 export const getAppointment = async (
-	appointmentId: string
+	appointmentId: string,
+	skipAuth: boolean = false
 ): Promise<AppointmentsDataInterface> => {
 	const url = endpoints.appointmentsServiceBase + '/' + appointmentId;
 
 	return fetchData({
 		url: url,
 		method: FETCH_METHODS.GET,
-		responseHandling: [FETCH_ERRORS.CATCH_ALL_WITH_RESPONSE]
+		responseHandling: [FETCH_ERRORS.CATCH_ALL_WITH_RESPONSE],
+		skipAuth: skipAuth
 	});
 };
--- src/api/videocalls/getJwt.ts
+++ src/api/videocalls/getJwt.ts
@@ -3,13 +3,15 @@ import { endpoints } from '../../resources/scripts/endpoints';
 import { FETCH_ERRORS, FETCH_METHODS, fetchData } from '../fetchData';
 
 export const getJwt = async (
-	appointmentId: string
+	appointmentId: string,
+	skipAuth: boolean = false
 ): Promise<VideoCallJwtDataInterface> => {
 	const url = endpoints.videocallServiceBase + '/' + appointmentId + '/jwt';
 
 	return fetchData({
 		url: url,
 		method: FETCH_METHODS.GET,
-		responseHandling: [FETCH_ERRORS.CATCH_ALL_WITH_RESPONSE]
+		responseHandling: [FETCH_ERRORS.CATCH_ALL_WITH_RESPONSE],
+		skipAuth: skipAuth
 	});
 };
