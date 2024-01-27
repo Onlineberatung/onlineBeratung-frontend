cd948eba589ff8bd46a2c9517215d63d2cec5751
Marc Itzenthaler <itzenthaler@web-mi.de>
2024-01-27 13:06:44 +0100

    feat: added extensions connecta

---
+++ src/extensions/components/legalInformationLinks/Privacy.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useTenant } from '../../../..';
+import { LegalPageWrapper } from '../legalPageWrapper/LegalPageWrapper';
+import useDocumentTitle from '../../utils/useDocumentTitle';
+
+export const Privacy = () => {
+	const [t] = useTranslation();
+	const tenant = useTenant();
+	useDocumentTitle(t('profile.footer.dataprotection'));
+	return (
+		<LegalPageWrapper
+			content={
+				tenant?.content?.privacy || t('profile.footer.dataprotection')
+			}
+			className={'terms'}
+		/>
+	);
+};
